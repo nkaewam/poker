d0cdd6e881aafb432ca3b3ac5b2dab37fe79dc2d
nkaewam <tontan2545@gmail.com>
2025-11-09 18:39:17 +0700

    fix: better url validation

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,10 +2,14 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/poker";
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
 
 // Disable prefetch as it's not supported in "Transaction" mode
-const client = postgres(connectionString, { prepare: false });
+const client = postgres(connectionString, { prepare: false, ssl: false });
 
 export const db = drizzle(client, { schema });
 
